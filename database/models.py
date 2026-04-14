c0395bc670a42f823c3a3e2a62ad6c12ddb097be
Inanatidaeboii <watsaphon.so@gmail.com>
2026-04-14 13:30:59 +0700

    [ Add ] Build React using Vite

--- database/models.py
+++ database/models.py
@@ -10,6 +10,7 @@ from sqlalchemy import (
     String,
     Text,
 )
+
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from pgvector.sqlalchemy import Vector
